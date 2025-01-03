13483f780383d489dfcd1e1033b635f2319a3244
Sunil Kumar <33151720+Milkhaa@users.noreply.github.com>
2025-01-03 19:59:32 +0530

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SnakeAndLadder from './components/SnakeAndLadder';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SnakeAndLadder />
+    </div>
+  );
+}
+
+export default App;
